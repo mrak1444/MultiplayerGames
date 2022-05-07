292fb9f98acfb11000c50bb65e1d89121534f5cb
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-05-07 21:06:19 +0600

    Выполнил Домашнее Задание 3 к уроку 6

--- Assets/Scripts/PhotonWorkingWithRooms/PhotonStart1.cs
+++ Assets/Scripts/PhotonWorkingWithRooms/PhotonStart1.cs
@@ -12,9 +12,12 @@ public class PhotonStart1 : MonoBehaviour, ILobbyCallbacks, IConnectionCallbacks
     [SerializeField] private List<Button> _buttons;
     [SerializeField] private List<Text> _buttonsText;
     [SerializeField] private Button _createRoom;
-    [SerializeField] private InputField _roomName;
+    [SerializeField] private InputField _roomNameForCreate;
     [SerializeField] private Text _textInfo;
     [SerializeField] private Button _closeRoom;
+    [SerializeField] private Button _invisibleRoom;
+    [SerializeField] private InputField _roomNameforJoin;
+    [SerializeField] private Button _joinRoom;
 
     private LoadBalancingClient _lbc;
     private TypedLobby _customLobby = new TypedLobby("customLobby", LobbyType.Default);
@@ -28,6 +31,11 @@ public class PhotonStart1 : MonoBehaviour, ILobbyCallbacks, IConnectionCallbacks
 
         _closeRoom.onClick.AddListener(CloseRoom);
         _closeRoom.interactable = false;
+
+        _joinRoom.onClick.AddListener(JoinRoom);
+
+        _invisibleRoom.onClick.AddListener(InvasibleRoom);
+        _invisibleRoom.interactable = false;
 
         _loadIcon.SetActive(true);
 
@@ -47,6 +55,7 @@ public class PhotonStart1 : MonoBehaviour, ILobbyCallbacks, IConnectionCallbacks
         var state = _lbc.State.ToString();
 
         if (_flag && !_lbc.CurrentRoom.IsOpen) _textInfo.text = "Room is close";
+        if (_flag && !_lbc.CurrentRoom.IsVisible) _textInfo.text = "Room is invisible";
     }
 
     private void UpdateCachedRoomList(List<RoomInfo> roomList)
@@ -97,7 +106,7 @@ public class PhotonStart1 : MonoBehaviour, ILobbyCallbacks, IConnectionCallbacks
         roomOptions.MaxPlayers = maxPlayers;
         EnterRoomParams enterRoomParams = new EnterRoomParams();
         enterRoomParams.Lobby = _customLobby;
-        enterRoomParams.RoomName = _roomName.text;
+        enterRoomParams.RoomName = _roomNameForCreate.text;
         enterRoomParams.RoomOptions = roomOptions;
         _lbc.OpCreateRoom(enterRoomParams);
     }
@@ -106,6 +115,18 @@ public class PhotonStart1 : MonoBehaviour, ILobbyCallbacks, IConnectionCallbacks
     {
         _lbc.CurrentRoom.IsOpen = false;
         
+    }
+
+    private void JoinRoom()
+    {
+        EnterRoomParams enterRoomParams = new EnterRoomParams();
+        enterRoomParams.RoomName = _roomNameforJoin.text;
+        _lbc.OpJoinRoom(enterRoomParams);
+    }
+
+    private void InvasibleRoom()
+    {
+        _lbc.CurrentRoom.IsVisible = false;
     }
 
     public void OnConnected()
@@ -185,6 +206,7 @@ public class PhotonStart1 : MonoBehaviour, ILobbyCallbacks, IConnectionCallbacks
     {
         _textInfo.text = "JoinedRoom!";
         _closeRoom.interactable = true;
+        _invisibleRoom.interactable = true;
         _createRoom.interactable = false;
         _flag = true;
         Debug.Log("14");
