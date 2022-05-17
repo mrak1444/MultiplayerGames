531b7db902a07d55c018d4c65a33c7a777eee336
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-05-17 19:42:29 +0600

    Выполнил Домашнее Задание 3 к уроку 7

--- Assets/Scripts/Lesson7/StartGame.cs
+++ Assets/Scripts/Lesson7/StartGame.cs
@@ -4,13 +4,62 @@ using Photon.Realtime;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
+using Photon.Pun.Demo.PunBasics;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class StartGame : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject _player;
 
+    private PlayerManager _playerManager;
+
     private void Start()
     {
-        PhotonNetwork.Instantiate(_player.name, new Vector3(UnityEngine.Random.Range(-5, 5), 0, UnityEngine.Random.Range(-5, 5)), Quaternion.identity);
+        var player = PhotonNetwork.Instantiate(_player.name, new Vector3(UnityEngine.Random.Range(-5, 5), 0, UnityEngine.Random.Range(-5, 5)), Quaternion.identity);
+        _playerManager = player.GetComponent<PlayerManager>();
+        
+        GetUserData("B37A5");
+    }
+
+    private void GetUserData(string myPlayFabId)
+    {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest()
+        {
+            PlayFabId = null,
+            Keys = null
+        }, result => {
+            Debug.Log("Got user data:");
+
+            if (result.Data.Count == 0)
+            {
+                SetUserData();
+            }
+            else
+            {
+                var value = result.Data["Health"].Value;
+                Debug.Log("Health: " + value);
+                _playerManager.Health = float.Parse(value);
+            }
+
+        }, (error) => {
+            Debug.Log("Got error retrieving user data:");
+            Debug.Log(error.GenerateErrorReport());
+        });
+    }
+
+    private void SetUserData()
+    {
+        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest()
+        {
+            Data = new Dictionary<string, string>() {
+                {"Health", _playerManager.Health.ToString()},
+            }
+        },
+        result => Debug.Log("Successfully updated user data"),
+        error => {
+            Debug.Log("Got error setting user data Ancestor to Arthur");
+            Debug.Log(error.GenerateErrorReport());
+        });
     }
 }
--- Assets/Scripts/Lesson7/StartLobby.cs
+++ Assets/Scripts/Lesson7/StartLobby.cs
@@ -4,11 +4,26 @@ using Photon.Realtime;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class StartLobby : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private Button _buttonPanelCreateAccount;
+
     [SerializeField] private Button _buttonConnect;
     [SerializeField] private InputField _inputFieldNickName;
+    [SerializeField] private InputField _inputFieldPasword;
+
+    [SerializeField] private Button _buttonCreateAccount;
+    [SerializeField] private Button _buttonCreateAccountBack;
+    [SerializeField] private InputField _inputFieldNickNameCreateAccount;
+    [SerializeField] private InputField _inputFieldEmailCreateAccount;
+    [SerializeField] private InputField _inputFieldPaswordCreateAccount;
+
+    [SerializeField] private GameObject _panelSignIn;
+    [SerializeField] private GameObject _panelCreateAccount;
+
     [SerializeField] private byte maxPlayers = 4;
 
     private RoomOptions _roomOptions = new RoomOptions();
@@ -18,14 +33,65 @@ public class StartLobby : MonoBehaviourPunCallbacks
     private void Start()
     {
         _buttonConnect.onClick.AddListener(ConnectToServer);
+        _buttonPanelCreateAccount.onClick.AddListener(PanelAccountPlayFab);
+        _buttonCreateAccount.onClick.AddListener(CreateAccountPlayFab);
+        _buttonCreateAccountBack.onClick.AddListener(PanelCreateAccountBack);
+
         PhotonNetwork.NickName = _inputFieldNickName.text;
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.GameVersion = "1";
     }
 
+    private void PanelCreateAccountBack()
+    {
+        _panelSignIn.SetActive(true);
+        _panelCreateAccount.SetActive(false);
+        _buttonPanelCreateAccount.gameObject.SetActive(true);
+    }
+
+    private void CreateAccountPlayFab()
+    {
+        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
+        {
+            Username = _inputFieldNickNameCreateAccount.text,
+            Email = _inputFieldEmailCreateAccount.text,
+            Password = _inputFieldPaswordCreateAccount.text,
+            RequireBothUsernameAndEmail = true
+        }, result =>
+        {
+            _panelSignIn.SetActive(true);
+            _panelCreateAccount.SetActive(false);
+            _buttonPanelCreateAccount.gameObject.SetActive(true);
+            Debug.Log($"Success: {_inputFieldNickNameCreateAccount.text}");
+        }, error =>
+        {
+            Debug.LogError($"Fail: {error.ErrorMessage}");
+        });
+    }
+
+    private void PanelAccountPlayFab()
+    {
+        _panelSignIn.SetActive(false);
+        _panelCreateAccount.SetActive(true);
+        _buttonPanelCreateAccount.gameObject.SetActive(false);
+    }
+
     private void ConnectToServer()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        
+
+        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
+        {
+            Username = _inputFieldNickName.text,
+            Password = _inputFieldPasword.text
+        }, result =>
+        {
+            Debug.Log($"Success: {_inputFieldNickName.text}");
+            PhotonNetwork.ConnectUsingSettings();
+        }, error =>
+        {
+            Debug.LogError($"Fail: {error.ErrorMessage}");
+        });
     }
 
     public override void OnConnectedToMaster()
