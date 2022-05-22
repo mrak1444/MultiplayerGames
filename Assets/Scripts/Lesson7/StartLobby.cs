a46a129df3c085533a0e657a950e09e9ea947acf
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-05-22 18:31:18 +0600

    Выполнил Домашнее Задания к уроку 8

--- Assets/Scripts/Lesson7/StartLobby.cs
+++ Assets/Scripts/Lesson7/StartLobby.cs
@@ -9,6 +9,7 @@ using PlayFab.ClientModels;
 
 public class StartLobby : MonoBehaviourPunCallbacks
 {
+    [Header("Account")]
     [SerializeField] private Button _buttonPanelCreateAccount;
 
     [SerializeField] private Button _buttonConnect;
@@ -23,12 +24,26 @@ public class StartLobby : MonoBehaviourPunCallbacks
 
     [SerializeField] private GameObject _panelSignIn;
     [SerializeField] private GameObject _panelCreateAccount;
+    [SerializeField] private GameObject _panelAccount;
 
+    [Header("Character")]
+    [SerializeField] private Button[] _buttonsAddCharacter;
+    [SerializeField] private Button[] _buttonsSelectCharacter;
+    [SerializeField] private GameObject _panelCharacter;
+    [SerializeField] private GameObject _pamelCreateCharacter;
+    [SerializeField] private InputField _inputFieldNameCreateCharacter;
+    [SerializeField] private Button _buttonCreateCharacter;
+    [SerializeField] private Button _buttonBack;
+
+    [Header("Other")]
+    [SerializeField] private GameObject _loaderIcon;
     [SerializeField] private byte maxPlayers = 4;
 
     private RoomOptions _roomOptions = new RoomOptions();
     private TypedLobby _customLobby = new TypedLobby("customLobby", LobbyType.Default);
     private string _roomName = "room1";
+    private List<CharacterResult> _characters = new List<CharacterResult>();
+    private string _itemId = "character_token";
 
     private void Start()
     {
@@ -37,9 +52,126 @@ public class StartLobby : MonoBehaviourPunCallbacks
         _buttonCreateAccount.onClick.AddListener(CreateAccountPlayFab);
         _buttonCreateAccountBack.onClick.AddListener(PanelCreateAccountBack);
 
-        PhotonNetwork.NickName = _inputFieldNickName.text;
+        for (int i = 0; i < _buttonsAddCharacter.Length; i++)
+        {
+            _buttonsAddCharacter[i].onClick.AddListener(() => { _pamelCreateCharacter.SetActive(true); });
+        }
+
+        _buttonBack.onClick.AddListener(() => { _pamelCreateCharacter.SetActive(false); });
+        _buttonCreateCharacter.onClick.AddListener(CreateCharacter);
+
+        //
+    }
+
+    private void CreateCharacter()
+    {
+        _loaderIcon.SetActive(true);
+
+        Debug.Log("CreateCharacter");
+
+        PlayFabClientAPI.PurchaseItem(new PurchaseItemRequest
+        {
+            CatalogVersion = "1",
+            ItemId = _itemId,
+            Price = 1,
+            VirtualCurrency = "GD"
+        }, Debug.Log, Debug.LogError);
+
+        PlayFabClientAPI.GrantCharacterToUser(new GrantCharacterToUserRequest
+        {
+            CharacterName = _inputFieldNameCreateCharacter.text,
+            ItemId = _itemId
+        }, result =>
+        {
+            UpdateCharacterStatistics(result.CharacterId);
+        }, Debug.LogError);
+
+        _pamelCreateCharacter.SetActive(false);
+    }
+
+    private void UpdateCharacterStatistics(string characterId)
+    {
+        Debug.Log("UpdateCharacterStatistics");
+        PlayFabClientAPI.UpdateCharacterStatistics(new UpdateCharacterStatisticsRequest
+        {
+            CharacterId = characterId,
+            CharacterStatistics = new Dictionary<string, int>
+            {
+                {"Level", 1},
+                {"XP", 0},
+                {"Gold", 0}
+            }
+        }, result =>
+        {
+            Debug.Log($"Initial stats set, telling client to update character list");
+            //CloseCreateNewCharacterPrompt();
+            GetCharacters();
+        },
+        Debug.LogError);
+    }
+
+    private void GetCharacters()
+    {
+        PlayFabClientAPI.GetAllUsersCharacters(new ListUsersCharactersRequest(),
+        res =>
+        {
+            Debug.Log($"Characters owned: + {res.Characters.Count}");
+            if (res.Characters.Count > 0)
+            {
+                if (_characters.Count > 0)
+                {
+                    _characters.Clear();
+                }
+
+                foreach (var character in res.Characters)
+                {
+                    _characters.Add(character);
+                }
+
+                var num = _characters.Count < 2 ? _characters.Count : 2;
+
+                for (int i = 0; i < num; i++)
+                {
+                    var name = _characters[i].CharacterName;
+
+                    _buttonsSelectCharacter[i].onClick.AddListener(delegate { StartGame(name); });
+
+                    _buttonsSelectCharacter[i].gameObject.SetActive(true);
+                    _buttonsAddCharacter[i].gameObject.SetActive(false);
+                    _buttonsSelectCharacter[i].transform.GetChild(0).GetComponent<Text>().text = name;
+                    
+                    UpdeteCharacterView(_characters[i].CharacterId, i);
+                }
+            }
+            else
+            {
+                _loaderIcon.SetActive(false);
+            }
+
+        }, Debug.LogError);
+    }
+
+    private void StartGame(string nameCharacter)
+    {
+        PhotonNetwork.NickName = nameCharacter;
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void UpdeteCharacterView(string characterId, int i)
+    {
+        PlayFabClientAPI.GetCharacterStatistics(new GetCharacterStatisticsRequest
+        {
+            CharacterId = characterId
+        }, result =>
+        {
+            _buttonsSelectCharacter[i].transform.GetChild(1).GetComponent<Text>().text = result.CharacterStatistics["Level"].ToString();
+            _buttonsSelectCharacter[i].transform.GetChild(2).GetComponent<Text>().text = result.CharacterStatistics["XP"].ToString();
+            _buttonsSelectCharacter[i].transform.GetChild(3).GetComponent<Text>().text = result.CharacterStatistics["Gold"].ToString();
+
+            _loaderIcon.SetActive(false);
+        }, Debug.LogError);
     }
 
     private void PanelCreateAccountBack()
@@ -78,8 +210,10 @@ public class StartLobby : MonoBehaviourPunCallbacks
 
     private void ConnectToServer()
     {
+        _loaderIcon.SetActive(true);
+        _panelAccount.SetActive(false);
+        _panelCharacter.SetActive(true);
         
-
         PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
         {
             Username = _inputFieldNickName.text,
@@ -87,7 +221,8 @@ public class StartLobby : MonoBehaviourPunCallbacks
         }, result =>
         {
             Debug.Log($"Success: {_inputFieldNickName.text}");
-            PhotonNetwork.ConnectUsingSettings();
+            GetCharacters();
+            //PhotonNetwork.ConnectUsingSettings();
         }, error =>
         {
             Debug.LogError($"Fail: {error.ErrorMessage}");
@@ -97,6 +232,7 @@ public class StartLobby : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster");
+
         _roomOptions.MaxPlayers = maxPlayers;
         PhotonNetwork.JoinOrCreateRoom(_roomName, _roomOptions, _customLobby);
     }
